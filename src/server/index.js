875b47440591e233c4950783b0a4a8db06b8e90a
Zenbook Eduardo <aeduardoiran@gmail.com>
2026-01-15 16:20:10 -0800

    feat: Version 1.0

---
+++ src/server/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+// Puerto
+app.set("port", process.env.PORT || 3000);
+
+// Configuraciones del cors
+const corsOptions = {
+    origin: [
+        "http://localhost:3000",
+        "http://localhost:8080",
+        "http://10.10.250.31:3000",
+        "http://10.10.250.31:3030",
+        "http://10.10.250.31:8080",
+        "http://10.10.250.31:8090"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+
+// Rutas
+
+// Endpoint para validar la actividad del server
+app.use("serverAlive", (req, res) => {
+    res.status(200).json({ message: `Servidor corriendo en el puerto ${app.get("port")}` })
+})
+
+// Servidor
+app.listen(app.get("port"), () => {
+    console.log(`Server corriendo en el puerto ${app.get("port")}`);
+})
