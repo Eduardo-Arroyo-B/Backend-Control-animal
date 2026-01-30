c9351a86ad9eb6a1f657e9c1f0028274f6b6b8a8
Zenbook Eduardo <aeduardoiran@gmail.com>
2026-01-30 00:15:24 -0800

    fix: Ajustes del cors

--- src/server/index.js
+++ src/server/index.js
@@ -38,7 +38,8 @@ const corsOptions = {
         "http://10.10.250.31:8080",
         "http://10.10.250.31:8090",
         "https://sica-roan.vercel.app",
-        "http://10.10.250.31:3060"
+        "http://10.10.250.31:3060",
+        "http://10.10.250.31:9060",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
