aff48c538c0347fe78008a88bc4aaf83e22a3c41
Zenbook Eduardo <aeduardoiran@gmail.com>
2026-02-09 15:52:56 -0800

    fix: Ajuste para la subida que soporta el back

--- src/server/index.js
+++ src/server/index.js
@@ -55,8 +55,8 @@ const corsOptions = {
 }
 
 // Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cors(corsOptions));
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
