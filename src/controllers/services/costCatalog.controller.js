211e76a7b46bb28c5d159afd7ed1af0985da3c77
Zenbook Eduardo <aeduardoiran@gmail.com>
2026-02-16 11:08:16 -0800

    feat: catalogo costos con logica y rutas correspondientes

---
+++ src/controllers/services/costCatalog.controller.js
@@ -0,0 +1,75 @@
+import prisma from "../../../prisma/prismaClient.js";
+
+const getAllCosts = async (req, res) => {
+    try {
+        const catalog = await prisma.tabla_Costos.findMany()
+
+        if (catalog.length === 0) {
+            return res.status(200).json({ message: "No hay datos de costos actualmente"});
+        }
+
+        return res.status(200).json({ message: "Catalogo de costos obtenido exitosamente", catalog });
+    } catch (error) {
+        return res.status(500).json({ message: "Ha ocurrido un error al obtener el catalogo de costos", error: error.message });
+    }
+}
+
+const createCost = async (req, res) => {
+    // Extraer datos del body
+    const {
+        nombre_servicio,
+        costo,
+        servicio_id     // ID del servicio (vacuna_id, esterilizaciones_id, o desparasitaciones_id)
+    } = req.body;
+
+    try {
+        // Lógica para asignar el id correspondiente al servicio basado en el tipo de servicio
+        let data = {};
+
+        if (nombre_servicio === 'Vacuna') {
+            data = {
+                nombre_servicio,
+                costo: Number(costo),
+                vacuna_id: servicio_id // Asignamos el id de la vacuna
+            };
+        } else if (nombre_servicio === 'Esterilizacion') {
+            data = {
+                nombre_servicio,
+                costo: Number(costo),
+                esterilizaciones_id: servicio_id // Asignamos el id de la esterilización
+            };
+        } else if (nombre_servicio === 'Desparacitacion') {
+            data = {
+                nombre_servicio,
+                costo: Number(costo),
+                desparasitaciones_id: servicio_id // Asignamos el id de la desparacitación
+            };
+        } else {
+            // Si el tipo de servicio no es válido
+            return res.status(400).json({
+                message: "Tipo de servicio no válido. Debe ser 'Vacuna', 'Esterilización' o 'Desparacitación'."
+            });
+        }
+
+        // Crear el nuevo costo en la base de datos
+        const cost = await prisma.tabla_Costos.create({
+            data,
+        });
+
+        return res.status(201).json({
+            message: "Costo creado exitosamente",
+            cost,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: "Ha ocurrido un error al crear el costo",
+            error: error.message,
+        });
+    }
+};
+
+
+export {
+    getAllCosts,
+    createCost,
+}
--- src/server/index.js
+++ src/server/index.js
@@ -28,6 +28,7 @@ import bitacora from "../routes/binnacle/binaccle.routes.js"
 import citas from "../routes/schedule/schedule.routes.js"
 import pelaje from "../routes/hair/hair.routes.js"
 import cita from "../routes/date/date.routes.js"
+import costos from "../routes/services/costCatalog.routes.js"
 
 // App
 const app = express();
@@ -88,7 +89,8 @@ app.use("/sica",
     bitacora,
     citas,
     pelaje,
-    cita
+    cita,
+    costos
 );
 
 // Endpoint para validar la actividad del server
