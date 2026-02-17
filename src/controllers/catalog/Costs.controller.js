1b9c4161f48b6fe1cedf05385c282b50ea401739
Eduardo Arroyo <aeduardoiran@gmail.com>
2026-02-16 23:35:01 -0800

    fix: ajustes a catalogo costos

--- src/controllers/catalog/Costs.controller.js
+++ src/controllers/catalog/Costs.controller.js
@@ -1,6 +1,18 @@
 import prisma from "../../../prisma/prismaClient.js";
 
-const getAllCosts = async (req, res) => {}
+const getAllCosts = async (req, res) => {
+    try {
+        const costos = await prisma.tabla_Costos.findMany()
+
+        if (costos.length === 0) {
+            return res.status(404).json({ message: "No existen datos actualmente en el catalogo de costos" })
+        }
+
+        return res.status(200).json({ message: "Catalogo de costos obtenido exitosamente", costos });
+    } catch (error) {
+        return res.status(500).json({ message: "Ha ocurrido un error al obtener la tabla de costos", error: error.message });
+    }
+}
 
 const createCost = async (req, res) => {
     // Extraer datos del body
@@ -10,39 +22,21 @@ const createCost = async (req, res) => {
         servicio_id     // ID del servicio (vacuna_id, esterilizaciones_id, o desparasitaciones_id)
     } = req.body;
 
+    // Objeto
+    const data = {
+        nombre_servicio,
+        costo,
+    }
+
     try {
-        // Lógica para asignar el id correspondiente al servicio basado en el tipo de servicio
-        let data = {};
-
-        if (nombre_servicio === 'Vacuna') {
-            data = {
-                nombre_servicio,
-                costo: Number(costo),
-                vacuna_id: Number(servicio_id) // Asignamos el id de la vacuna
-            };
-        } else if (nombre_servicio === 'Esterilizacion') {
-            data = {
-                nombre_servicio,
-                costo: Number(costo),
-                esterilizaciones_id: Number(servicio_id) // Asignamos el id de la esterilización
-            };
-        } else if (nombre_servicio === 'Desparacitacion') {
-            data = {
-                nombre_servicio,
-                costo: Number(costo),
-                desparasitaciones_id: Number(servicio_id) // Asignamos el id de la desparacitación
-            };
-        } else {
-            // Si el tipo de servicio no es válido
-            return res.status(400).json({
-                message: "Tipo de servicio no válido. Debe ser 'Vacuna', 'Esterilización' o 'Desparacitación'."
-            });
-        }
-
         // Crear el nuevo costo en la base de datos
         const cost = await prisma.tabla_Costos.create({
             data,
         });
+
+        if (!cost) {
+            return res.status(400).json({ message: "Ha ocurrido un error al crear el costo" });
+        }
 
         return res.status(201).json({
             message: "Costo creado exitosamente",
