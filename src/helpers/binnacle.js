5009b7fbf3aa4fb4b7dd5fc68c26cc97d8315aad
Zenbook Eduardo <aeduardoiran@gmail.com>
2026-02-20 15:48:28 -0800

    fix: ajustes a todas las fechas de las bitacoras

--- src/helpers/binnacle.js
+++ src/helpers/binnacle.js
@@ -3,10 +3,13 @@ import prisma from "../../prisma/prismaClient.js"
 // Funcion para crear un registro en la bitacora
 export default async function binnacle({usuarioId, fecha_hora, operacion, ip, resultado}) {
     try {
+
+        const localNow = new Date().toLocaleString("es-MX", { timeZone: "America/Tijuana" });
+
         const log = await prisma.bitacora_Auditoria.create({
             data: {
                 usuarioId,
-                fecha_hora,
+                fecha_hora: localNow,
                 operacion,
                 ip,
                 resultado
